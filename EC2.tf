04b1d938e389df509bcb7a98ea29664e9e02b253
pklab-git <76607415+pklab-git@users.noreply.github.com>
2020-12-28 19:24:41 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -5,7 +5,7 @@ provider "aws" {
   /*version = ">= 2.20"*/
 }
 
-resource "aws_instance" "myec2-b" {
+resource "aws_instance" "mynewec2" {
    ami = "ami-07dd19a7900a1f049"
    instance_type = "t2.micro"
 }
